54b00d9c6ba344bd8008276f4315ce2fbc5295b2
IS <1588186+issarapong@users.noreply.github.com>
2023-05-10 16:04:41 +0700

    setup express

---
+++ lab02/app.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const app =express()
+
+const port = process.env.PORT || 8080
+app.listen(port,()=> console.log(`Server on ${port}`))
+
+
