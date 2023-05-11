4aff619dd6f921b1f3f98b00df179b72f420ed8e
IS <1588186+issarapong@users.noreply.github.com>
2023-05-11 10:36:12 +0700

    DELETE By ID

--- hw1/README.md
+++ hw1/README.md
@@ -1,4 +1,6 @@
-pagination by limit
+# PAGINATION
+
+## GET pagination by limit
 
 
 http://localhost:8888/products?_page=2&_limit=3
--- hw1/app.js
+++ hw1/app.js
@@ -6,19 +6,60 @@ const app = express()
 
 //const getProducts = () => readFile("./products.json", "utf8").then(data => JSON.parse(data)) //JSON.parse(data) GET STRING to json obj
 const getProducts = () => readFile("./products.json", "utf8").then(JSON.parse) 
+const saveFile = (file, data) => writeFile(file, JSON.stringify(data, null, 2))
+
 
 app.get('/products',(req,res)=> {
      const {_page = 1, _limit = 10} = req.query
      getProducts().then(allproduct => {
         console.log(allproduct)
         let start = (_page-1) * _limit
-        let end = start + _limit
+        let end = start + +_limit
         let scope_items = allproduct.slice(start, end)
         console.log(scope_items)
         return scope_items
      }).then( output => res.json(output))
   
 })
+
+
+app.delete('/products/:id', (req,res)=> {
+    const {id} =req.params
+    getProducts().then(allproduct => {
+        let del_idx = allproduct.findIndex(el => el.id === +id)
+        if(del_idx === -1)
+        return {}
+        let [del_item] = allproduct.splice(del_idx, 1)
+        //SaveToDelete
+
+        return { allproduct, del_item}
+
+    }).then( ({allproduct, del_item}) => {
+        if(del_item)
+              saveFile('./products.json', allproduct)
+              return { msg: `${del_item?.id || 'no thing'} have deleted`}
+
+    }).then( msg => res.json(msg))
+})
+
+// app.delete('/product/:id', (req, res) => {
+//     const {id} = req.params
+//     getProducts().then(all => {
+//         let del_idx = all.findIndex( el => el.id === +id)
+//         if(del_idx === -1) 
+//             return {}
+//         let [del_item] = all.splice(del_idx, 1)
+//         // saveToDeleted
+//         return {all, del_item}
+//     }).then( ({all, del_item}) => {
+//         if(del_item)
+//             saveFile('./products.json', all)
+//         return { msg: `${del_item?.id || 'nothing'} have deleted `}
+//     }).then( msg => res.json(msg))
+// })
+
+
+
 
 app.use((req,res)=> {
     res.status(404).json({ msg: "path not found"})
