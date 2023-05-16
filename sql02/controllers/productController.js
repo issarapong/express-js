92cd296830d322757aa579a34edeb7c189c11873
IS <1588186+issarapong@users.noreply.github.com>
2023-05-16 16:10:03 +0700

    Get by query CLI

--- sql02/controllers/productController.js
+++ sql02/controllers/productController.js
@@ -1,7 +1,19 @@
-exports.getAllProducts = (req, res, next) => {
-    res.send(' in getAllProducts')
+const mysql = require('mysql2/promise')
+
+const pool = mysql.createPool(process.env.DB_CONNECT)
+
+// exports.getAllProducts = (req, res, next) => {
+//     res.send(' in getAllProducts')
     
+// }
+
+exports.getAllProducts = (req, res, next) => {
+    let sql ='select * from products'
+    pool.query(sql).then(([rows]) => {
+        res.json(rows)
+    }).catch(next)
 }
+
 
 exports.getProductById = (req, res, next) => {
     res.send('in GetProduct By Id')
